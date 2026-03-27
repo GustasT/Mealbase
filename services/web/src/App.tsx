a3ba6a829d63c1163aa06063733a653c3715bd44
GustasT <102315449+GustasT@users.noreply.github.com>
2026-03-27 15:23:30 +0200

    frontend(web): setup Vite + Tailwind, remove default styles

---
+++ services/web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Tailwind veikia</h1>
+    </div>
+  );
+}
+
+export default App;
